c0a33b133540a8f5434a9b36db62d9da58726e16
SujayM <SujayMane@Hotmail.com>
2021-01-16 03:25:06 +0530

    Sequelize - Added User module

---
+++ models/user.js
@@ -0,0 +1,21 @@
+const sequelize = require("sequelize");
+
+const sequelizeObj = require("../util/database");
+
+const User = sequelizeObj.define({
+  id: {
+      type: sequelize.STRING,
+      allowNull : false,
+      primaryKey : true
+  },
+  name:{
+      type: sequelize.STRING,
+      allowNull : false
+  },
+  email: {
+      type: sequelize.STRING,
+      allowNull : false 
+  }
+});
+
+module.exports = User;
