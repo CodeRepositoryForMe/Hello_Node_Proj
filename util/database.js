6bcf4c8ed730c71c6f28dead53dcd36fa1b1c44a
SujayM <SujayMane@Hotmail.com>
2021-01-15 10:06:00 +0530

    Created DB connection through sequelize

--- util/database.js
+++ util/database.js
@@ -1,15 +1,29 @@
-const mysql = require("mysql2");
-const dotenv = require('dotenv');
+//const mysql = require("mysql2");
+const dotenv = require("dotenv");
+const sequelize = require("sequelize");
 
 dotenv.config();
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PWD
-});
+//// Traditional MySQL connection
 
-module.exports = pool.promise();
- 
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     database: process.env.DB_NAME,
+//     password: process.env.DB_PWD
+// });
+
+// module.exports = pool.promise();
+
+//// Changs for sequelize
+
+const sequelizeObj = new sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PWD,
+  { dialect: process.env.DB_SERVER, 
+    host: process.env.DB_HOST }
+);
+
+module.exports = sequelizeObj;
